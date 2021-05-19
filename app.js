388e8d139faf0478e699c3fdce12b226df35d9f9
cesar juarez <cesar.juarez@codeimmersives.com>
2021-05-19 08:34:06 -0700

    code

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require ('express');
+const logger = require ('morgan');
+const path = require ("path");
+
+
+const app = express();
+
+app.use (logger('dev'));
+app.use (express.json());
+app.use (express.static(path.join(__dirname, "public")))
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", 'ejs');
+
+app.get ('/', function(req, res){
+    res.render ('index',{user: "cesar",
+    info: ["likes to watch a movie", "read", "eat"]
+})
+});
+app.get("/photo-fun", function (req, res){
+    res.render ('photos');
+});
+app.get ("/:pet/:age", function(req, res){
+    res.render({pet: req.params.pet, age: req.params.age})
+})
+app.listen (3000, function (){
+    console.log ("server started in port 3000");
+})
